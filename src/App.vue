562a42b6d510e438d01e2bf906f2e2b62cb9dc72
Mustafa Demir <mustafademir_md@hotmail.com>
2022-11-25 15:33:17 +0300

    h5p standalone player

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div ref="container"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue"
+const container = ref()
+const options = {
+  h5pJsonPath:  '/public/VideoIntroduce',
+  frameJs: 'public/player/frame.bundle.js',
+  frameCss: 'public/player/styles/h5p.css',
+}
+onMounted(() => {
+  new H5PStandalone.H5P(container.value, options);
+})
+</script>
